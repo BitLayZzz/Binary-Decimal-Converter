58ecb22871918cdc4843f4cfd1057bdc2c3bda26
BitLayZzz <blackburton23@gmail.com>
2021-03-15 19:27:52 -0700

    Removed trailing whitespaces

--- converter.rb
+++ converter.rb
@@ -2,10 +2,10 @@
 
 ########### Two's Complement Logic ##########
 
-def add_bit(working_arr, og_bin_arr) 
+def add_bit(working_arr, og_bin_arr)
   working_arr[-1] += 1
 
-  (0...og_bin_arr.length).each do |i| 
+  (0...og_bin_arr.length).each do |i|
     if working_arr[-i - 1] > 1
 
       working_arr[-i - 2] += 1
@@ -19,7 +19,7 @@ end
 
 def bit_flip(working_arr, og_bin_arr)
   working_arr.each_index do |b|
-    
+
     working_arr[b] == 0 ? working_arr[b] = 1 : working_arr[b] = 0
 
   end
@@ -32,7 +32,7 @@ end
 ############# Decimal to Binary ############
 
 def dec_twos_complement(og_bin_arr, og_num)
-  
+
   working_arr = Array.new(og_bin_arr)
 
   loop do
@@ -42,19 +42,19 @@ def dec_twos_complement(og_bin_arr, og_num)
   end
 
   if og_num < 0
-    final_result = bit_flip(working_arr, og_bin_arr) 
+    final_result = bit_flip(working_arr, og_bin_arr)
   else
     final_result = working_arr
   end
 
-  final_result 
+  final_result
 end
 
 def dec_2_bin(num)
   bin_arr = Array.new
 
   new_num = num.to_i.abs
-  
+
   loop do
     remainder = new_num % 2
 
@@ -69,7 +69,7 @@ def dec_2_bin(num)
 
   twos_comp_result = dec_twos_complement(bin_arr, num)
 
-  loop do 
+  loop do
     break if twos_comp_result.first == 1
 
     twos_comp_result.delete_at(0)
@@ -89,13 +89,13 @@ def sign_bit?(og_arr)
 end
 
 def bin_twos_complement(bin)
-  bin_arr = bin.to_s.split('').map(&:to_i) 
+  bin_arr = bin.to_s.split('').map(&:to_i)
 
   loop do
     break if bin_arr.length >= 16
 
     bin_arr.unshift(0)
-  end 
+  end
 
   bin_arr
 end
@@ -115,11 +115,11 @@ end
 def bin_2_dec(bin_arr, sign_bit)
   dec_num = 0
 
-  working_arr = Array.new(bin_arr) 
+  working_arr = Array.new(bin_arr)
 
   if sign_bit == '+'
     dec_num = bin_2_dec_math(bin_arr)
-    
+
     return dec_num
   else
     bin_arr = bit_flip(working_arr, bin_arr)
@@ -138,7 +138,7 @@ def bin_number?(num)
 
   num_split.each do |digit|
 
-    return -1 if digit.to_i < 0 || digit.to_i > 1 
+    return -1 if digit.to_i < 0 || digit.to_i > 1
 
   end
 
@@ -169,11 +169,11 @@ when 2
   printf "=> Enter in a number:  "
 
   bin_num = gets.chomp.to_i
-  
-  binary_number_check = bin_number?(bin_num) 
+
+  binary_number_check = bin_number?(bin_num)
 
   bin_arr = bin_twos_complement(bin_num)
-  
+
   if binary_number_check < 0
     puts "Number is not a binary number..."
 
@@ -188,7 +188,7 @@ when 2
     puts "Ending Program..."
   else
     sign_bit = sign_bit?(bin_arr)
- 
+
     dec_num = bin_2_dec(bin_arr, sign_bit)
 
     puts "=> Binary: #{bin_num}\n=> Decimal: #{dec_num}"
